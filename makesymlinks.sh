f36d11aeb4bd6c13892c2439018e6c6910cb8217
olsonbg <olsonbg@gmail.com>
2015-03-31 11:19:11 -0500

    Added Xresources.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,8 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash.d vimrc vim"   # list of files/folders to symlink in homedir
+# list of files/folders to symlink in homedir
+files="bashrc bash.d vimrc vim Xresources"
 
 ##########
 
