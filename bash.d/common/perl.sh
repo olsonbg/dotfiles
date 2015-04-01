b8bec10c4ac7cda3b48b35f1dfcc4dd167beb1a7
olsonbg <olsonbg@gmail.com>
2015-03-31 21:07:56 -0500

    Add perl env variables for local perl modules

---
+++ bash.d/common/perl.sh
@@ -0,0 +1,5 @@
+PATH="/home/bgo/perl5/bin${PATH+:}${PATH}"; export PATH;
+PERL5LIB="/home/bgo/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/home/bgo/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/home/bgo/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/home/bgo/perl5"; export PERL_MM_OPT;
