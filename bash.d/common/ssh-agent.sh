cbce976be1282f6596faacfe75901e6d9d9320c6
olsonbg <olsonbg@gmail.com>
2015-05-30 22:18:55 -0500

    Enable ssh-agent on host talon.

--- bash.d/common/ssh-agent.sh
+++ bash.d/common/ssh-agent.sh
@@ -1,12 +1,11 @@
 SSH_ENV="$HOME/.ssh/environment"
 
-function start_agent {
+function start_ssh_agent {
 #    echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
 #    echo succeeded
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add;
 }
 
 # Source SSH settings, if applicable
@@ -15,8 +14,8 @@ if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
     #ps ${SSH_AGENT_PID} doesn't work under cywgin
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
+        start_ssh_agent;
     }
 else
-    start_agent;
+    start_ssh_agent;
 fi
