a5516d6958a04b0d40f5bba376370bbdc709f46d
olsonbg <olsonbg@gmail.com>
2015-06-09 15:35:27 -0500

    Added solarized-dark and solarized-light bash functions to set the current color scheme.

--- bash.d/common/solarized.sh
+++ bash.d/common/solarized.sh
@@ -3,31 +3,11 @@
 solarized_theme() {
 	SOLARIZED_THEME=$(basename $(readlink ~/.Xresources))
 	export SOLARIZED_THEME=${SOLARIZED_THEME/Xresources-/}
-
-
-	# Make sure the dircolors theme matches the current
-	# solarized theme (dark/light).
-	if [ "$SOLARIZED_THEME" == "dark" ]; then
-		eval $(dircolors ~/.bash.d/themes/dircolors.ansi-light)
-	elif [ "$SOLARIZED_THEME" == "light" ]; then
-		eval $(dircolors ~/.bash.d/themes/dircolors.ansi-dark)
-	fi
 }
 
-#Toggle solarized light and dark themes.
-solarized() {
-	if [ "$SOLARIZED_THEME" == "dark" ]; then
-		ln -sf ~/dotfiles/Xresources-light ~/.Xresources
-	elif [ "$SOLARIZED_THEME" == "light" ]; then
-		ln -sf ~/dotfiles/Xresources-dark ~/.Xresources
-	fi
-
-	if [ -n "$DISPLAY" ]; then
-		# Only run xrdb if in X.
-		xrdb ~/.Xresources
-	fi
-
+solarized-curr-colors() {
 	# Get the foreground, background, and cursor colors.
+	# from ~/.Xresources
 	local FG=$(sed -e '/^#define S_base0 /!d' -e 's/^#define S_base0 *//' ~/.Xresources)
 	local BG=$(sed -e '/^#define S_base03/!d' -e 's/^#define S_base03 *//' ~/.Xresources)
 	local CU=$(sed -e '/^#define S_base1 /!d' -e 's/^#define S_base1 *//' ~/.Xresources)
@@ -35,8 +15,45 @@ solarized() {
 
 	# Update the colors
 	echo -ne "${Solarized}"
+}
 
-	# Update the environmental variable and dircolors.
+solarized-dark() {
+	ln -sf ~/dotfiles/Xresources-dark ~/.Xresources
+	eval $(dircolors ~/.bash.d/themes/dircolors.ansi-dark)
+
+	if [ -n "$DISPLAY" ]; then
+		# Only run xrdb if in X.
+		xrdb ~/.Xresources
+	fi
+
+	solarized-curr-colors
+}
+
+solarized-light() {
+	ln -sf ~/dotfiles/Xresources-light ~/.Xresources
+	eval $(dircolors ~/.bash.d/themes/dircolors.ansi-light)
+
+	if [ -n "$DISPLAY" ]; then
+		# Only run xrdb if in X.
+		xrdb ~/.Xresources
+	fi
+
+	solarized-curr-colors
+}
+
+#Toggle solarized light and dark themes.
+solarized() {
+
+	[[ -n "$SOLARIZED_THEME" ]] && solarized_theme
+
+	if [ "$SOLARIZED_THEME" == "dark" ]; then
+		solarized-light
+	elif [ "$SOLARIZED_THEME" == "light" ]; then
+		solarized-dark
+	fi
+
+
+	# Update the environmental variable.
 	solarized_theme
 }
 
