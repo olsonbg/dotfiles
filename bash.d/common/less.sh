0d799982429cf1b2dbf895db84400e939b8f878d
olsonbg <olsonbg@gmail.com>
2015-09-18 19:42:54 -0500

    Added colors to 'less,' to make match more closely to the solarized scheme.

--- bash.d/common/less.sh
+++ bash.d/common/less.sh
@@ -1,2 +1,7 @@
-export LESS="-R -M"
 export LESSOPEN="|lesspipe.sh %s"
+export LESSCOLOR="always"
+export LESS_TERMCAP_so=$(tput setaf 3)$(tput setab 7)
+export LESS_TERMCAP_se=$(tput sgr0)
+export LESS="-R -M"
+
+# vim: ft=sh
