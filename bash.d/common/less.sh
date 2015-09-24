d145726b1384956fddae089a481fe02722dd7250
olsonbg <olsonbg@gmail.com>
2015-09-24 12:52:55 -0500

    Really fix the checking of TERM problem.

--- bash.d/common/less.sh
+++ bash.d/common/less.sh
@@ -1,7 +1,7 @@
 export LESSOPEN="|lesspipe.sh %s"
 export LESSCOLOR="always"
 export LESS="-R -M"
-if [ -n "$TERM" ]; then
+if [ "$TERM" != "dumb" ]; then
 	export LESS_TERMCAP_so=$(tput setaf 3)$(tput setab 7)
 	export LESS_TERMCAP_se=$(tput sgr0)
 fi
