7e61ae25cefcbb76d8b607c7d085513a6c52819d
olsonbg <olsonbg@gmail.com>
2015-09-24 12:33:53 -0500

    Use tput only is TERM is defined

--- bash.d/common/less.sh
+++ bash.d/common/less.sh
@@ -1,7 +1,8 @@
 export LESSOPEN="|lesspipe.sh %s"
 export LESSCOLOR="always"
-export LESS_TERMCAP_so=$(tput setaf 3)$(tput setab 7)
-export LESS_TERMCAP_se=$(tput sgr0)
 export LESS="-R -M"
-
+if [ -n "$TERM" ]; then
+	export LESS_TERMCAP_so=$(tput setaf 3)$(tput setab 7)
+	export LESS_TERMCAP_se=$(tput sgr0)
+fi
 # vim: ft=sh
