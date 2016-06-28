b77ae33186fcfaac7c9def7f6eb80870094c21a8
olsonbg <olsonbg@gmail.com>
2016-06-28 08:13:03 -0500

    Fix gpg-agent startup script.
    Was not working when more than one terminal was in use.

--- bash.d/common/gpg-agent.sh
+++ bash.d/common/gpg-agent.sh
@@ -1,23 +1,25 @@
 GPG_ENV="$HOME/.gnupg/environment"
-
-function start_gpg_agent {
-#    echo "Initialising new GPG agent..."
-    /usr/bin/gpg-agent --daemon  --write-env-file "${GPG_ENV}" > /dev/null
-#    echo succeeded
-    chmod 600 "${GPG_ENV}"
-    . "${GPG_ENV}" > /dev/null
-}
-
-# Source GPG settings, if applicable
+unset GPG_AGENT_PID
 
 if [ -f "${GPG_ENV}" ]; then
-    . "${GPG_ENV}" > /dev/null
+	. "${GPG_ENV}" > /dev/null
 	GPG_AGENT_PID=$(echo $GPG_AGENT_INFO|sed -e 's/^.*S.gpg-agent:\(.*\):.*$/\1/')
-    #ps ${GPG_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${GPG_AGENT_PID} | grep "/usr/bin/gpg-agent" > /dev/null || {
-        start_gpg_agent;
-    }
-    unset GPG_AGENT_PID
-else
-    start_gpg_agent;
 fi
+
+# Check if program with pid of $GPG_AGENT_PID is gpg-agent
+[[ -f /proc/$GPG_AGENT_PID/cmdline ]] && cat /proc/$GPG_AGENT_PID/cmdline |\
+                                         grep "^$(which gpg-agent)" > /dev/null
+
+# If gpg-agent isn't running, start it.
+if [ $? -ne 0 ]; then
+#	echo "Initialising new GPG agent..."
+	/usr/bin/gpg-agent --daemon  --write-env-file "${GPG_ENV}" > /dev/null
+	chmod 600 "${GPG_ENV}"
+	. "${GPG_ENV}" > /dev/null
+fi
+
+export GPG_AGENT_INFO
+
+# Cleanup
+unset GPG_AGENT_PID
+unset GPG_ENV
