0da81c4478b619750897af369979c93cc9c11f5e
olsonbg <olsonbg@gmail.com>
2016-07-03 03:39:12 -0500

    Nearly complete rewrite of ssh-multi.sh
    Fix for when no 'user' is selected, so that settings in ~/.ssh/config can
    be used.
    
    Added option for including the localhost (-l option).
    
    Use tmux session name (default: sshmulti) for all tmux commands to avoid
    ambiguity.
    
    No longer opens an extra window when starting a new tmux session.
    
    Uses more sane exit codes.

--- bin/ssh-multi.sh
+++ bin/ssh-multi.sh
@@ -1,63 +1,107 @@
 #!/bin/bash
 
-# ssh-multi : a script to ssh multiple servers over multiple tmux panes
-# nomad-fr : https://github.com/nomad-fr/scripts-systems
+# ssh-multi : a script to ssh multiple servers over multiple tmux panes with
+#             with all panes syncronized.
+#
+# olsonbg   : bin/ssh-multi.sh @ https://github.com/olsonbg/dotfiles
+#
+# Based on nomad-fr  : https://github.com/nomad-fr/scripts-systems
+#
 # Based on D.Kovalov work : https://gist.github.com/dmytro/3984680
 
-# config
-#user=$USER # user use for ssh connection
-user=root
+session="sshmulti"
 
 usage() {
-    echo $1
-    echo
-    echo 'ssh-multi.sh : [OPTION]'
-    echo '   -u user                           : user use for ssh connection : default "root"'
-    echo '   -d "serv0 serv1 serv2 ... servN"  : list serv to connect to'
-    echo
-    echo '   Bonus:'
-    echo '   -d "$(echo 'serv'{0..3})" : is the same as : -d "serv0 serv1 serv2 serv3"'
-    echo '   -d "$(anotherscript)" : call a script that give a list of host separated by space'
-    exit 0
+	[[ -n "$1" ]] && echo -e "$1\n"
+	echo "$(basename $0) -d hosts [-l] [-u username]"
+	echo
+	echo '   -d "serv0 serv1 ... servN"  : list servers to connect to.'
+	echo '   -l                          : include localhost.'
+	echo '   -u user                     : user for ssh connection.'
+	echo
+	echo '  Tips:'
+	echo '   -d "$(echo serv{0..2})"'
+	echo '   -d "$(script)"'
 }
 
 starttmux() {
-    local hosts=( $HOSTS )
-    local target="multi-ssh ${host[0]}"
-    tmux new-window -n "${target}" ssh $user@${hosts[0]}
-    unset hosts[0];
-    for i in "${hosts[@]}"
-    do
-        tmux split-window -t :"${target}" -h "ssh $user@$i"
-        tmux select-layout -t :"${target}" tiled > /dev/null	
-    done
-    tmux select-pane -t 0
-    tmux set-window-option -t :"${target}"  synchronize-panes on > /dev/null
+	local hosts=( $HOSTS )
+	local launchedtmux=0
+
+	[[ -n "$user" ]] && user="$user@"
+
+	local window="ssh-multi $user${hosts[0]}"
+
+	if [ -z "$TMUX" ]; then # if not in a tmux session create one
+		if [ "$useLOCALHOST" = 1 ] ; then
+			tmux -u new-session -d -s "${session}" -n "${window}"
+		else
+			tmux -u new-session -d -s "${session}" -n "${window}" ssh "$user${hosts[0]}"
+			unset hosts[0];
+		fi
+
+		launchedtmux=1
+	else # Get session name of current tmux session.
+		session="$(tmux display-message -p '#S')"
+
+		if [ "$useLOCALHOST" = 1 ] ; then
+			tmux new-window -t "${session}" -n "${window}"
+		else
+			tmux new-window -t "${session}" -n "${window}" ssh $user${hosts[0]}
+			unset hosts[0];
+		fi
+		launchedtmux=0
+	fi
+
+	for i in "${hosts[@]}"
+	do
+			tmux split-window -t "${session}:${window}" -h "ssh $user$i"
+	done
+
+	tmux select-layout -t "${session}:${window}" tiled
+	tmux select-pane -t "${session}:${window}"
+	tmux set-window-option -t "${session}:${window}"  synchronize-panes on
+
+	# Setup complete. If we were already inside a tmux session, then we should
+	# be on the newly created window with synchronized panes, otherwise, we
+	# need to connect to the new tmux session.
+	if [ "$launchedtmux" = 1 ]; then tmux a -dt ${session}; fi
 }
 
 checkopt() {
-    if [ -z "$HOSTS" ]; then
-	usage "Please provide of list of hosts with -d option."
-    fi
-    if [ -z "$TMUX" ]; then # if not in a tmux session create one
-	tmux -u new-session -d -s multi-ssh
-	local launchtmux=1
-    fi
-    starttmux
-    if [ "$launchtmux" = 1 ]; then tmux a -dt multi-ssh; fi
+	if [ -z "$HOSTS" ]; then
+		usage "Please provide list of hosts with -d option."
+		return 1
+	fi
+
+	return 0
 }
 
-while getopts "u:d:h" o; do
-        case "${o}" in
-	    h)
-		usage
-		;;
-	    u)
-                user=${OPTARG}
-                ;;
-            d)
-                HOSTS=${OPTARG}
-                ;;
-        esac
+
+useLOCALHOST=0
+
+while getopts "u:d:lh" opts; do
+	case "${opts}" in
+		h)
+			usage
+			exit 0
+			;;
+		u)
+			user=${OPTARG}
+			;;
+		d)
+			HOSTS=${OPTARG}
+			;;
+		l)
+			useLOCALHOST=1
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
 done
-checkopt
+
+checkopt || exit 1
+
+starttmux
