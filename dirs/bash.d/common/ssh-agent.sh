7aaa5da74b1482e9fb316608d5eb1d2354139032
olsonbg <olsonbg@gmail.com>
2016-07-09 01:20:46 -0500

    Cleanup ssh-agent.sh script.

--- dirs/bash.d/common/ssh-agent.sh
+++ dirs/bash.d/common/ssh-agent.sh
@@ -1,21 +1,21 @@
 SSH_ENV="$HOME/.ssh/environment"
-
-function start_ssh_agent {
-#    echo "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-#    echo succeeded
-    chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
-}
+unset SSH_AGENT_PID
 
 # Source SSH settings, if applicable
 
 if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_ssh_agent;
-    }
-else
-    start_ssh_agent;
+	. "${SSH_ENV}" > /dev/null
+fi
+
+# Check if program with pid of $SSH_AGENT_PID is ssh-agent
+[[ -f /proc/$SSH_AGENT_PID/cmdline ]] && cat /proc/$SSH_AGENT_PID/cmdline |\
+                                         grep "^$(which ssh-agent)" > /dev/null
+
+# If ssh-agent isn't running, start it
+if [ $? -ne 0 ]; then
+	#echo "Initialising new SSH agent..."
+	/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+	#echo succeeded
+	chmod 600 "${SSH_ENV}"
+	. "${SSH_ENV}" > /dev/null
 fi
