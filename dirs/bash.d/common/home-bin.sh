0300c60c22c6800566737aeb94f7e5f2698788a0
olsonbg <olsonbg@gmail.com>
2016-12-06 12:10:09 -0600

    Only add ${HOME}/bin to path if not already there
    If ${HOME}/bin exists, and it is not already in PATH, append it to PATH.

--- dirs/bash.d/common/home-bin.sh
+++ dirs/bash.d/common/home-bin.sh
@@ -1,1 +1,13 @@
-[[ -d "${HOME}/bin" ]] && export PATH="${PATH}:${HOME}/bin"
+# Add ${HOME}/bin to PATH, if it exists.
+if [ -d "${HOME}/bin" ]; then
+	TPATH=""
+
+	if [ $(echo $PATH|grep -c "${HOME}/bin") == "0" ]; then
+		TPATH="$PATH:${HOME}/bin"
+	fi
+
+	[[ -n "$TPATH" ]] && export PATH="$TPATH"
+
+	unset TPATH
+fi
+
