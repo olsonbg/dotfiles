02343cbbca07cb5e80dd1eab03c99711ace779ab
olsonbg <olsonbg@gmail.com>
2018-09-21 10:30:56 -0500

    Set TERM_PROGRAM=urxvtd when TERM==rxvt
    TERM_PROGRAM is used by solarized vim plugin and neofetch.

--- dirs/bash.d/common/term.sh
+++ dirs/bash.d/common/term.sh
@@ -1,10 +1,10 @@
 # The solarized scheme of Vim looks at the variable TERM_PROGRAM to determine
 # if the terminal supports italic fonts. rxvt does support italics, so I set it
 # here, if I'm using rxvt.
-[[ "${TERM:0:4}" == "rxvt" ]] && export TERM_PROGRAM=rxvt
+[[ "${TERM:0:4}" == "rxvt" ]] && export TERM_PROGRAM=urxvtd
 # I use xft for xterm fonts, so it also supports italics. Some characters do
 # get cut off a little but, but that's okay with me. I rarely use xterm anyway.
-[[ "${TERM:0:5}" == "xterm" ]] && export TERM_PROGRAM=rxvt
+# [[ "${TERM:0:5}" == "xterm" ]] && export TERM_PROGRAM=urxvtd
 
 # Set TERM to a standard value, depending upon to color capabilities of the
 # terminal.
