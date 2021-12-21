bcc4efb2d9662c48509951419db9cf6171d9529a
olsonbg <olsonbg@gmail.com>
2021-12-21 11:49:18 -0600

    grace: set editor to vim in a urxvt terminal

--- dirs/bash.d/common/grace.sh
+++ dirs/bash.d/common/grace.sh
@@ -1,6 +1,6 @@
 export GRACE_PRINT_CMD="kprinter";
 #export GRACE_EDITOR="gvim";
-export GRACE_EDITOR="xterm -e vim";
+export GRACE_EDITOR="urxvtc -e vim";
 # Extended ascii characters in grace doesn't work with utf
 alias xmgrace='LANG="C" /usr/bin/xmgrace'
 
